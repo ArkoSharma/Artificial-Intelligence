cf3311116cadd15b7696581f513187913e8090c3
ArkoSharma <37324791+ArkoSharma@users.noreply.github.com>
2018-09-04 11:53:10 +0530

    Added first solution .

---
+++ Week 6/P1.py
@@ -0,0 +1,171 @@
+"""
+
+Arko Sharma
+111601002
+
+Lab 6 :
+03-09-2018
+         
+"""
+import numpy as np
+import Queue
+class Environment:
+      
+      """ 
+
+      The environment is initially in a state in which starting position is in the lower left corner.
+      The goal cell is the upper right corner.
+      The environment records the number of steps taken to search the goal.
+
+      There is an explored list which keeps track of the explored NODES of the program graph (denoted by the cell in which the agent is ).
+      """
+ 
+
+      def __init__(self,dimension):
+          
+	  self.dimension = dimension
+	  self.grid = np.zeros([dimension,dimension])
+          self.goal_r = 0
+          self.goal_c = dimension - 1
+	
+          #create diagonal blockages
+          for i in range (self.dimension) : 
+              for j in range (self.dimension) :
+                  if( i == j ):
+                      self.grid[i][j] = 1
+           	      if  ( j < dimension - 1) : 
+                          self.grid[i][j+1] = 1
+
+          self.grid[0][0] = 0
+          self.grid[0][1] = 0
+          for i in range(dimension):
+	      print self.grid[i]          
+      """ 
+      Function to indicate whether a given action is valid, given the starting position
+      """
+      def is_valid(self,curr_r,curr_c,act_r,act_c):
+          new_r = curr_r + act_r
+          new_c = curr_c + act_c
+          if  ( new_r >= 0 and new_r < self.dimension and new_c >= 0 and new_c < self.dimension and self.grid[new_r][new_c] != 1):
+              return True
+          else:
+              return False
+
+
+      """
+      The environment implements a  cost function which returns 1 for each action that moves
+      to a non-goal cell and 0 for those which do not.
+      """
+      def cost(self,curr_r,curr_c,act_r,act_c):
+          new_r = curr_r + act_r
+          new_c = curr_c + act_c
+          if  ( new_r == self.goal_r and new_c == self.goal_c):
+              return 0
+          else:
+              return 1
+
+
+      """
+      Runs a BFS to look for the goal-cell
+      """
+      def BFS_4(self):
+ 
+          dx = [1,0,-1,0]
+          dy = [0,-1,0,1]
+          explored_list = []
+          start = (self.dimension - 1,0)
+          Q = Queue.Queue()
+          Q.put(start)
+          while(not (Q.empty())):
+              
+	      s = Q.get()
+              explored = False
+
+              #check if the goal state has been reached
+              if (self.cost(s[0],s[1],0,0) == 0):
+                  explored_list.append(s)
+                  break    	                 
+
+
+	      for i in range (len(explored_list)):
+                  if( explored_list[i] == s ):
+                      explored = True
+                      break
+              if(not explored):
+                  for i in range(4):
+	              if ( self.is_valid(s[0],s[1],dx[i],dy[i])):
+                         #note that if the child is invalid OR EXPLORED BEFORE, then it is not pushed in the Queue
+                         if  ( (s[0] + dx[i], s[1] + dy[i]) not in explored_list ):
+         	             Q.put((s[0]+dx[i],s[1]+dy[i]))   	   
+
+              #after expanding all the children , mark the node as explored
+              explored_list.append(s)
+              if(explored):
+                  continue                            
+
+	  print("\nWith 4 Actions,Exploration takes place as follows : ")
+          temp = np.zeros([self.dimension,self.dimension])
+          for i in range(len(explored_list)):
+              current = explored_list[i]
+              temp[current[0]][current[1]] = 1
+	      print("Visiting {}".format(current))
+              print temp
+              print("########################")    
+
+          return ("For BFS with 4 actions, Goal reached in {} steps.".format(len(explored_list)))
+
+
+      """ 
+      BFS with diagonal moves possible
+      """
+      def BFS_8(self):
+ 
+          dx = [1,0,-1,0,1,1,-1,-1]
+          dy = [0,-1,0,1,1,-1,1,-1]
+          explored_list = []
+          start = (self.dimension - 1,0)
+          Q = Queue.Queue()
+          Q.put(start)
+          while(not (Q.empty())):
+              
+	      s = Q.get()
+              explored = False
+
+              #check if the goal state has been reached
+              if (self.cost(s[0],s[1],0,0) == 0):
+                  explored_list.append(s)
+                  break    	                 
+
+
+	      for i in range (len(explored_list)):
+                  if( explored_list[i] == s ):
+                      explored = True
+                      break
+              if(not explored):
+                  for i in range(8):
+	              if ( self.is_valid(s[0],s[1],dx[i],dy[i])):
+                         #note that if the child is invalid OR EXPLORED BEFORE, then it is not pushed in the Queue
+                         if  ( (s[0] + dx[i], s[1] + dy[i]) not in explored_list ):
+         	             Q.put((s[0]+dx[i],s[1]+dy[i]))   	   
+
+              #after expanding all the children , mark the node as explored
+              explored_list.append(s)
+              if(explored):
+                  continue                            
+
+	  print("\nWith 8 Actions, Exploration takes place as follows : ")
+          temp = np.zeros([self.dimension,self.dimension])
+          for i in range(len(explored_list)):
+              current = explored_list[i]
+              temp[current[0]][current[1]] = 1
+	      print("Visiting {}".format(current))
+              print temp
+              print("########################")    
+
+          return ("For BFS with 8 actions, Goal reached in {} steps.".format(len(explored_list)))
+
+myenv = Environment(10)
+msg4_ = (myenv.BFS_4())
+msg8_ = (myenv.BFS_8())
+print msg4_
+print msg8_
