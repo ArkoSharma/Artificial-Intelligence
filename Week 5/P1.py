177388c193c08e79bcd0269f5b48324467b89171
ArkoSharma <37324791+ArkoSharma@users.noreply.github.com>
2018-09-16 23:54:34 +0530

    Updated P1.py

--- Week 5/P1.py
+++ Week 5/P1.py
@@ -1,13 +1,13 @@
 """
-
 Arko Sharma
 111601002
-
-Lab 4 :: ( n-squared minus 1 puzzle )
+Lab 5 :: ( n-squared minus 1 puzzle ) - Solution using BFS
          
 """
 
 import numpy as np
+import Queue
+import copy
 class N2Puzzle:
       """ 
       Initialise the environment with a state which 8 random steps away from the goal destination.
@@ -30,13 +30,30 @@ class N2Puzzle:
           # the number n-squared denotes the empty square whose location is tracked in the environment.
 
           self.n = n
-	  self.board = np.array(self.get_random_permutation()).reshape(self.n,self.n)
+	  self.board = np.array([x for x in range(self.n * self.n)]).reshape(self.n,self.n)
+	  k = 1
+	  for i in range(self.n):
+	      for j in range(self.n):
+	          self.board[i][j] = k
+	          k += 1
           
 	  for i in range (self.n):
               for j in range(self.n):
                   if( self.board[i][j] == self.n*self.n ) :
                       self.empty_i = i
                       self.empty_j = j
+          
+
+      def is_solved(self):
+          #function to check if the current state represents the goal state ie a solved board.
+          #for doing this, just check if the board is sorted while traversing in row-major order.         
+          k = 1          
+          for i in range(self.n):
+              for j in range(self.n):
+                  if  (self.board[i][j] != k):
+                      return False
+                  k += 1
+          return True           
 
 
       def move_empty_square(self,move_string):
@@ -60,7 +77,7 @@ class N2Puzzle:
                    self.empty_j -= 1
 
           if(move_string == "down" ):
-              if(self.empty_i == n-1):
+              if(self.empty_i == self.n-1):
                    return
               else :
                    temp = self.board[self.empty_i+1][self.empty_j]
@@ -69,7 +86,7 @@ class N2Puzzle:
                    self.empty_i += 1
 
           if(move_string == "right" ):
-              if(self.empty_j == n-1):
+              if(self.empty_j == self.n-1):
                    return
               else :
                    temp = self.board[self.empty_i][self.empty_j + 1]
@@ -79,38 +96,17 @@ class N2Puzzle:
                    
 
 
-      def gen_xstep_board(self,x):
-	  #generates a board that is x random steps away from the destination
-	  next = 1
-	  for i in range (n):
-              for j in range(n):
-                  self.board[i][j] = next
- 		  next += 1
-
-	  prev_direction = -1
-	  for i in range (x):
-              direction =  get_random_2D_direction
-	      if(direction != prev_direction):
-	         prev_direction = direction
-	         if( direction == 0 ):
-                    move_empty_square("up")
-                 if( direction == 1):
-                    move_empty_square("down")
-                 if( direction == 2):
-                    move_empty_square("left")
-                 if( direction == 3): 
-                    move_empty_square("right")
-
-	      else i -= 1
-              
- 	  self.empty_i = self.n - 1
-          self.empty_j = self.j - 1
-	  
+      def gen_xstep_board(self,x_steps):
+	  #generates a board that is at most "x_steps" moves away from the destination
+          direction = ["up", "down", "left", "right"]
+          for i in range(x_steps):
+              d = np.random.randint(4)
+              self.move_empty_square(direction[d])
+	  print("{}".format(self.board)) 
 
 
       def show_parity(self):
-          #function to calculate and show the parity as defined in the question
-          
+          #function to calculate and show the parity as defined in the question.
 	  #bottom right is in location(n-1,n-1)
           d_s = (self.n-1-self.empty_i) + (self.n - 1 - self.empty_j)
           counter = 0
@@ -126,8 +122,69 @@ class N2Puzzle:
           print("Parity = {}".format((d_s + counter)%2))
           print("Empty location = ({},{})".format(self.empty_i,self.empty_j))
           print("{}".format(self.board)) 
-                           
 
-new_puzzle = N2Puzzle(2)
-new_puzzle.show_parity()
-            
+
+
+      def get_hash(self):
+    	  #function to hash the board; ie create a string that uniquely determines the state of the board
+    	  str = ""
+          for i in range(self.n):
+              for j in range(self.n):
+                str += chr(ord('a') + self.board[i][j] - 1)
+          return str
+                 
+""" 
+Solver class.
+Currently only one 'method' of solving : BFS.
+"""
+class N2Solver :
+
+      def __init__(self):
+          self.path = []
+          
+
+      def BFS(self,puzzle):
+          
+          
+          #function to solve the given N^2 - 1 puzzle using BFS 
+          directions = ["up", "right", "left", "down"]
+          #npuz = InhN2Puzzle(puz)
+          #define visited list,action-dict,parent-list and a Queue for BFS 
+          vis = []
+          action = {}
+          Q = Queue.Queue()
+          parent = {}          
+          #Insert start state into the queue.
+          #Set the parent of the start state to itself.
+          Q.put(puzzle)
+          parent[puzzle.get_hash()] = puzzle.get_hash()
+          while Q:
+          
+              curr = Q.get()
+              if  curr.is_solved():
+                  #Upon reaching the goal, reconstruct the path and return it.
+                  current = curr.get_hash()
+                  while(parent[current] != current):
+                      self.path.append(action[current])
+                      current = parent[current]
+                  self.path = self.path[::-1]
+                  return self.path
+
+              for Dir in directions:
+                  #create a copy of the original puzzle, apply movement, and insert into Queue
+                  childPuzzle = copy.deepcopy(curr)
+                  childPuzzle.move_empty_square(Dir)
+                  if  childPuzzle.get_hash() in vis:
+                      continue
+                  else:
+                      vis.append(childPuzzle.get_hash())
+                      parent[childPuzzle.get_hash()] = curr.get_hash()
+                      action[childPuzzle.get_hash()] = Dir
+                      Q.put(childPuzzle)
+          
+
+new_puzzle = N2Puzzle(4)
+new_puzzle.gen_xstep_board(4)
+new_solver = N2Solver()
+print "\nSolution is:"
+print new_solver.BFS(new_puzzle)
