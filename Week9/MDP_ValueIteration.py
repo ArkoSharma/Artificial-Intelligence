3c856855b524eea618417f773f080fea5d0b423b
ArkoSharma <37324791+ArkoSharma@users.noreply.github.com>
2018-10-02 00:03:22 +0530

    Updated MDP_ValueIteration.py

--- Week9/MDP_ValueIteration.py
+++ Week9/MDP_ValueIteration.py
@@ -4,6 +4,13 @@ Arko Sharma
 Program depicting Value iteration and Policy iteration in a grid world environment.
 Here the reward is a function of the current state only ie a constant reward is assumed to
 be obtained upon reaching a state.
+
+
+issues :
+       probabilities of illegal moves to be el.
+       check value iteration and do viBottUP
+       do policy iteration
+       blocks
 """
 from __future__ import print_function
 import numpy as np
@@ -32,7 +39,12 @@ class MDP_grid():
         self.gamma = gamma
         self.T = {}
         self.R = {}
-        self.actions = ["u","d","l","r"]
+        self.actions  = ["u","d","l","r"]
+        self.dxdy_act = {}
+        self.dxdy_act["u"] = (-1,0)
+        self.dxdy_act["d"] = (1,0)
+        self.dxdy_act["l"] = (0,-1)
+        self.dxdy_act["r"] = (0,1)
         self.states = []
         for i in range (self.dimension):
             for j in range (self.dimension):
@@ -46,7 +58,7 @@ class MDP_grid():
         #Here, R is only a function of state ie each state has a fixed incoming reward.
         
         for s in states:
-            self.R[s] = np.random.randint(-100,100)
+            self.R[s] = np.random.randint(-10,11)
         print ("Rewards:")
         k = 0
         for s in states:
@@ -55,63 +67,52 @@ class MDP_grid():
             if (k + 1 % 4 == 0):
                 print("")
                 k = 0
+
+    def inside_board(self,state):
+        r,c = state.r,state.c
+        if(r >= 0 and r < self.dimension and c >=0 and c < self.dimension):
+            return True
+        else: 
+            return False
+
+    def get_state(self,some_state):
+        #Function to return that state of the MDP which has the given state values of the state variables
+        for s in self.states:
+            if (s.r == some_state.r and s.c == some_state.c):
+                return s
         
     def generate_random_TransitionProbabilityMatrix(self,states,actions):
-        # Function used to generated P.
+        # Function used to generate T.
         # Assumed that the agent moves only to the 4 adjoining cells with non zero probability.
-        # Movement to any state that is does not share a side with current is 0.
-        for s in states:
-            
-            #up
-            success = np.random.uniform(0.7,1.0)
-            if (s.r == 0):
-                success = 0             
-            remaining = 1.0 - success
-            fail = remaining/3
-            self.T[("u",s) ] = []
-            self.T[("u",s) ].append((state(s.r - 1, s.c),success))
-            self.T[("u",s) ].append((state(s.r + 1, s.c),fail))
-            self.T[("u",s) ].append((state(s.r , s.c - 1),fail))
-            self.T[("u",s) ].append((state(s.r , s.c - 1),fail))
+        # Movement to any state(cell) that does not share a side with current is impossible.
 
-            #down
-            success = np.random.uniform(0.7,1.0)
-            if (s.r == self.dimension - 1): 
-                success = 0             
-            remaining = 1.0 - success
-            fail = remaining/3
-            self.T[("d",s) ] = []
-            self.T[("d",s) ].append((state(s.r - 1, s.c),fail))
-            self.T[("d",s) ].append((state(s.r + 1, s.c),success))
-            self.T[("d",s) ].append((state(s.r , s.c - 1),fail))
-            self.T[("d",s) ].append((state(s.r , s.c - 1),fail))
-            
-            #left
-            success = np.random.uniform(0.7,1.0)
-            if (s.c == 0): 
-                success = 0             
-            remaining = 1.0 - success
-            fail = remaining/3
-            self.T[("l",s) ] = []
-            self.T[("l",s) ].append((state(s.r - 1, s.c),fail))
-            self.T[("l",s) ].append((state(s.r + 1, s.c),fail))
-            self.T[("l",s) ].append((state(s.r , s.c - 1),success))
-            self.T[("l",s) ].append((state(s.r , s.c - 1),fail))
-            
-            #right        
-            success = np.random.uniform(0.7,1.0)
-            if (s.c == self.dimension - 1): 
-                success = 0             
-            success = np.random.uniform(0.7,1.0)
-            remaining = 1.0 - success
-            fail = remaining/3
-            self.T[("r",s) ] = []
-            self.T[("r",s) ].append((state(s.r - 1, s.c),fail))
-            self.T[("r",s) ].append((state(s.r + 1, s.c),fail))
-            self.T[("r",s) ].append((state(s.r , s.c - 1),fail))
-            self.T[("r",s) ].append((state(s.r , s.c - 1),success))
-               
+        dx = [1,0,-1,0]
+        dy = [0,1,0,-1]
         
+
+        for s in states:
+            for a in actions:
+                valid_neighbours = []
+
+                success     = np.random.uniform(0.7,1.0)
+                succ_neigh  = state(self.dxdy_act[a][0] + s.r , self.dxdy_act[a][1] + s.c)
+                if(not(self.inside_board(succ_neigh))):
+                    success = 0
+          
+                for x in dx:
+                    for y in dy:
+                        neighbour = state(s.r + x, s.c + y)
+                        if (self.inside_board(neighbour) and neighbour != succ_neigh):
+                            valid_neighbours.append(self.get_state(neighbour))
+               
+                fail          = (1 - success)/len(valid_neighbours)
+                self.T[(a,s)] = []
+                for val_neigh in valid_neighbours:
+                    self.T[(a,s)].append((val_neigh,fail))
+                
+                if (success != 0):
+                    self.T[(a,s)].append((self.get_state(succ_neigh),success))                 
+                                                     
         
 class SolveMDP:
     
@@ -122,8 +123,10 @@ class SolveMDP:
     def __init__(self):
         pass
   
-    def value_iteration(self,mdp_grid):
-        """Solving an MDP by value iteration. [Figure 17.4]"""
+    def value_iteration(self,mdp_grid,epsilon):
+        """Solving an MDP by value iteration.
+           Starting with random V and iterating till convergence.
+        """
 
         #define random Values	
         V = {}
@@ -131,25 +134,47 @@ class SolveMDP:
         T,R,gamma = mdp_grid.T,mdp_grid.R,mdp_grid.gamma
 
         for s in mdp_grid.states:
-            V[s] = np.random.randint(-1000,1001)
+            V[s] = np.random.randint(-10,11)
        
         while(True):
             V_copy = V.copy()
             delta = 0
             for s in mdp_grid.states:
                 Q = []
-                
                 for a in mdp_grid.actions:
-                    for (next_state, prob) in T[(a,s)])
-                        sum += gamma * (prob * V_copy[next_state]))
-                    Q.append(R[s] + sum, 
+                    acc = 0
+                    for (next_state, prob) in T[(a,s)]:
+                        acc += prob*V_copy[next_state]
+                    Q.append(R[s] + gamma*acc) 
                 V[s] = np.max(Q)
-                policy[s] = np.argmax[Q] 
+                policy[s] = np.argmax(Q) 
+
+
+                for i in range (mdp_grid.dimension):
+                    for j in range(mdp_grid.dimension):
+                        print ("{} ".format(V[mdp_grid.get_state(state(i,j))]),end = "")
+                    print("")
+                print("\n\n")
                 
+
             delta = max(delta, np.abs(V[s] - V_copy[s]))
             if  delta <= epsilon*(1 - gamma)/gamma:
                 return policy
 
+
+                
+    def value_iterationBottomUp(self,mdp_grid,epsilon):
+        """Solving an MDP by value iteration.
+           Building table bottom up.
+        """
+        #Fill Table
+
+
 grid     = MDP_grid(4,0.9)
 solver   = SolveMDP()
-print(solver.value_iteration(grid))
+policy   = (solver.value_iteration(grid,0.00001))
+for i in range (4):
+    for j in range(4):
+        print("{} ".format(grid.actions[policy[grid.get_state(state(i,j))]]),end = "")
+    print("")   
+
